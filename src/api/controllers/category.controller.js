35177f866be0b985486d839f4f4cf9f332314a1a
Sopheak Sok-Macbook Pro <soksopheak669@gmail.com>
2022-04-20 23:12:59 +0700

    Finish cleaning create and update image

--- src/api/controllers/category.controller.js
+++ src/api/controllers/category.controller.js
@@ -2,10 +2,8 @@ const formidable = require('formidable');
 
 const httpStatus = require('http-status');
 // const { omit } = require('lodash');
-const fs = require('fs');
 const Category = require('../models/category.model');
 const APIError = require('../errors/api-error');
-const constants = require('../../config/constants');
 const myUtils = require('../utils/myUtils');
 /**
  * Load category and append to req.
@@ -54,33 +52,8 @@ exports.create = async (req, res, next) => {
       const category = new Category(fields);
 
       if (!files.length) { // check if single file
-        const file = files.icon;
-        const { newFilename, filepath } = file;
-
-        // Check if the file is valid
-        const isValid = isFileValid(file);
-
-        // creates a valid name by removing spaces
-        const fileName = `${newFilename}.${file.mimetype.split('/').pop()}`;
-        if (!isValid) {
-          throw new APIError({
-            message: 'File type is not supported',
-            status: httpStatus.UNSUPPORTED_MEDIA_TYPE,
-          });
-        }
-        // Rename the file in the directory
-        // const saveToPath = `./images/category/${fileName}`;
-        const saveToPath = `${constants.publicDir}/images/category/${fileName}`;
-        try {
-          // store filename to folder
-          fs.writeFileSync(saveToPath, fs.readFileSync(filepath));
-        } catch (error) {
-          throw new APIError({
-            message: error,
-            status: httpStatus.UNSUPPORTED_MEDIA_TYPE,
-          });
-        }
-        category.icon = `/images/category/${fileName}`;
+        const storedIconUrl = myUtils.storeSingleImage(files.icon, 'category');
+        category.icon = storedIconUrl;
       }
 
       const savedCategory = await category.save();
@@ -90,15 +63,6 @@ exports.create = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-};
-
-const isFileValid = (file) => {
-  const type = file.mimetype.split('/').pop();
-  const validTypes = ['jpg', 'jpeg', 'png'];
-  if (validTypes.indexOf(type) === -1) {
-    return false;
-  }
-  return true;
 };
 
 // /**
@@ -138,34 +102,9 @@ exports.update = async (req, res, next) => {
         category.set(fields);
 
         if (!files.length) { // check if single file
-          const file = files.icon;
-          const { newFilename, filepath } = file;
-
-          // Check if the file is valid
-          const isValid = isFileValid(file);
-
-          // creates a valid name by removing spaces
-          const fileName = `${newFilename}.${file.mimetype.split('/').pop()}`;
-          if (!isValid) {
-            throw new APIError({
-              message: 'File type is not supported',
-              status: httpStatus.UNSUPPORTED_MEDIA_TYPE,
-            });
-          }
-          // Rename the file in the directory
-          // const saveToPath = `./images/category/${fileName}`;
-          const saveToPath = `${constants.publicDir}/images/category/${fileName}`;
-          try {
-            // store filename to folder
-            fs.writeFileSync(saveToPath, fs.readFileSync(filepath));
-          } catch (error) {
-            throw new APIError({
-              message: error,
-              status: httpStatus.UNSUPPORTED_MEDIA_TYPE,
-            });
-          }
+          const storedIconUrl = myUtils.storeSingleImage(files.icon, 'category');
           myUtils.removeSingleImage(`./src/public${category.icon}`);
-          category.icon = `/images/category/${fileName}`;
+          category.icon = storedIconUrl;
         }
 
         const savedCategory = await category.save();
